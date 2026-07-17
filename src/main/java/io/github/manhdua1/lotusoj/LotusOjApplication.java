8190650e2b154a01306e95c033813c8da5be2c55
manhdua1 <manhvu20012005@gmail.com>
2026-07-17 18:19:10 +0700

    Initial commit

---
+++ src/main/java/io/github/manhdua1/lotusoj/LotusOjApplication.java
@@ -0,0 +1,13 @@
+package io.github.manhdua1.lotusoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotusOjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotusOjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/manhdua1/lotusoj/LotusOjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.manhdua1.lotusoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotusOjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
