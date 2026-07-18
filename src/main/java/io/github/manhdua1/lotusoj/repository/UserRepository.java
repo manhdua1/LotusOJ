e7cc9d4c0987d49ff78340eb835c03425abfd115
manhdua1 <manhvu20012005@gmail.com>
2026-07-18 22:14:11 +0700

    feat(user): add UserRepository

---
+++ src/main/java/io/github/manhdua1/lotusoj/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package io.github.manhdua1.lotusoj.repository;
+
+import io.github.manhdua1.lotusoj.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
