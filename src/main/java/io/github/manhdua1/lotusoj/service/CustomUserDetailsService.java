8b2f384010fe30414ffdb86580117c44de8413a9
manhdua1 <manhvu20012005@gmail.com>
2026-07-22 16:55:02 +0700

    fix(security): fix cycle dependency injection

--- src/main/java/io/github/manhdua1/lotusoj/service/CustomUserDetailsService.java
+++ src/main/java/io/github/manhdua1/lotusoj/service/CustomUserDetailsService.java
@@ -10,7 +10,9 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CustomUserDetailsService implements UserDetailsService {
