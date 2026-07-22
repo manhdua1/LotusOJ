31b979781d5bb5ef227472169994de03570aad0b
manhdua1 <manhvu20012005@gmail.com>
2026-07-22 13:17:22 +0700

    feat(auth): implement UserDetailsService

--- src/main/java/io/github/manhdua1/lotusoj/repository/UserRepository.java
+++ src/main/java/io/github/manhdua1/lotusoj/repository/UserRepository.java
@@ -3,7 +3,9 @@ package io.github.manhdua1.lotusoj.repository;
 import io.github.manhdua1.lotusoj.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
