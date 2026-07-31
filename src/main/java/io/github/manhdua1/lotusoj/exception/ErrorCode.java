7f4f92476cc7fe5fe9ad915c076ca2cd596583cd
manhdua1 <manhvu20012005@gmail.com>
2026-07-31 10:36:23 +0700

    fix(auth): INVALID_USERNAME message

--- src/main/java/io/github/manhdua1/lotusoj/exception/ErrorCode.java
+++ src/main/java/io/github/manhdua1/lotusoj/exception/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
-    INVALID_USERNAME(1003, "Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
+    INVALID_USERNAME(1003, "Invalid username", HttpStatus.BAD_REQUEST),
     INVALID_PASSWORD(1004, "Password must be at least 6 characters", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
