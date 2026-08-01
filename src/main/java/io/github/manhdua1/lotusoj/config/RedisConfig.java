9c1c5c0e6d00cb21add7fc2016b9b27f1b1f148c
manhdua1 <manhvu20012005@gmail.com>
2026-08-01 15:52:15 +0700

    fix: redis host and port value

--- src/main/java/io/github/manhdua1/lotusoj/config/RedisConfig.java
+++ src/main/java/io/github/manhdua1/lotusoj/config/RedisConfig.java
@@ -12,10 +12,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
-    @Value("${redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
 
